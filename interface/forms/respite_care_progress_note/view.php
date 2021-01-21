13a43bbe15f4d4aed383209f139f610d3aafb222
hermie <hermiebarit@gmail.com>
2021-01-21 11:39:05 +0800

    Adding respite eSign button

--- interface/forms/respite_care_progress_note/view.php
+++ interface/forms/respite_care_progress_note/view.php
@@ -14,10 +14,12 @@
  * @copyright Copyright (C) 2017 Stephen Waite <stephen.waite@cmsvt.com>
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
+$folderName = 'respite_care_progress_note';
 $formid = 0 + (isset($_GET['id']) ? $_GET['id'] : 0);
-if(isset($_SESSION['formID_' . $formid ])){
+$table_id = $_SESSION['formID_' . $folderName .'_'. $formid];
+if(!empty($table_id)){
     require("readonly.php");
-} else {
+} else {    
     require("new.php");
 }
 
