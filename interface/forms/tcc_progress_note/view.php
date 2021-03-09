2bf610f8d109421c5dbe3811e0b3ffa879c58b6e
hermie <hermiebarit@gmail.com>
2021-03-09 20:05:49 +0800

    Adding eSign and Print button in TCC Progress Note

--- interface/forms/tcc_progress_note/view.php
+++ interface/forms/tcc_progress_note/view.php
@@ -8,6 +8,11 @@
  * @copyright Copyright (c) 2015 Z&H Consultancy Services Private Limited <sam@zhservices.com>
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
-
-
-require("new.php");
+$folderName = 'tcc_progress_note';
+$formid = 0 + (isset($_GET['id']) ? $_GET['id'] : 0);
+$table_id = $_SESSION['formID_' . $folderName .'_'. $formid];
+if(!empty($table_id)){
+    require("readonly.php");
+} else {    
+    require("new.php");
+}
